d9d565e087dfb9a6d80e9269bce9c597b458146e
JonasSchenker <jonas.schenker@stud.uni-heidelberg.de>
2025-05-06 18:38:53 +0200

    Made split_channels work

--- Code/Functions/KMeans.py
+++ Code/Functions/KMeans.py
@@ -1,14 +1,13 @@
 #Function to extract the single colour channels of the image (wahrscheinlich unnötig)
 
-def split_channels(image):
+def split_channels(img_array):
      
      if img_array.ndim != 3 or img_array.shape[2] != 3:
         raise ValueError("Das Eingabebild muss die Form (H, W, 3) haben (RGB).")
      else:
     #reshape image to dimensions (hight*with of image, 3(dimension of colours R, G and B))
-        reshaped_image = image.reshape(-1, 3)
-        image.R = reshaped_image[:,0]
-        image.G = reshaped_image[:,1]
-        image.B = reshaped_image[:,2]
-
-        return image.R, image.G, image.B
+        reshaped_image = img_array.reshape(-1, 3)
+        R = reshaped_image[:,0]
+        G = reshaped_image[:,1]
+        B = reshaped_image[:,2]
+        return R, G, B
