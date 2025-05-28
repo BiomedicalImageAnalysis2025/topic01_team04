e054edfdf077489c5d0f529f4b80ab96a84b51f1
JonasSchenker <jonas.schenker@stud.uni-heidelberg.de>
2025-05-28 09:20:30 +0200

    Implemented First KMeans Algorithm (RGB)

--- Code/Functions/KMeans.py
+++ Code/Functions/KMeans.py
@@ -22,9 +22,8 @@ def init_centroids(k):
    centroids = np.random.rand(k, 3)  # RGB-Werte zwischen 0 und 1
    return centroids
 
-centroids = init_centroids(3)
-centroids
 
+#(
 # Function to calculate euclidean distance in a 3D space
 def euclidean_distance_3d(p1, p2):
     """
@@ -40,3 +39,96 @@ def euclidean_distance_3d(p1, p2):
     p1 = np.array(p1)
     p2 = np.array(p2)
     return np.sqrt(np.sum((p1 - p2) ** 2))
+#) --> already implemented in assign_to_centroids
+
+
+# Function to assign each pixel to the nearest centroid
+def assign_to_centroids(pixels, centroids):
+    """
+    Weist jedem Pixel den Index des nächstgelegenen Zentroiden zu.
+    
+    Parameter:
+    - pixels (ndarray): (n, 3) RGB-normalisierte Pixel
+    - centroids (ndarray): (k, 3) aktuelle Zentroiden
+    
+    Rückgabe:
+    - labels (ndarray): (n,) Array mit Index des jeweils nächsten Zentroids
+    """
+    n = pixels.shape[0]
+    k = centroids.shape[0]
+    labels = np.empty(n, dtype=int)
+    
+    for i in range(n):
+        # Berechne euklidische Distanz zu allen k Zentroiden
+        distances = np.sqrt(np.sum((centroids - pixels[i])**2, axis=1))
+        labels[i] = np.argmin(distances)
+    
+    return labels
+
+
+# Function to update centroids based on assigned pixels
+def update_centroids(pixels, labels, k):
+    """
+    Aktualisiert die Zentroiden basierend auf den zugewiesenen Pixeln.
+    
+    Parameter:
+    - pixels (ndarray): (n, 3) RGB-normalisierte Pixel
+    - labels (ndarray): (n,) Array mit Index des jeweils nächsten Zentroids
+    - k (int): Anzahl der Zentroiden
+    
+    Rückgabe:
+    - new_centroids (ndarray): (k, 3) aktualisierte Zentroiden
+    """
+    new_centroids = np.zeros((k, 3))
+    
+    for i in range(k):
+        # Extrahiere alle Pixel, die dem i-ten Zentroiden zugewiesen sind
+        assigned_pixels = pixels[labels == i]
+        
+        if len(assigned_pixels) > 0:
+            new_centroids[i] = np.mean(assigned_pixels, axis=0)
+        else:
+            new_centroids[i] = np.random.rand(3)  # Zufälliger Wert, falls kein Pixel zugewiesen ist
+    
+    return new_centroids
+
+
+# Function to perform K-Means clustering
+def kmeans_clustering(image, k, max_iterations=100):
+    """
+    Perform K-means clustering on a 3D image.
+
+    Parameters:
+    - image: 3D numpy array representing the image.
+    - k: Number of clusters.
+    - max_iterations: Maximum number of iterations for convergence.
+
+    Returns:
+    - segmented_image: 3D numpy array with clustered pixel values.
+    """
+    # Reshape Image
+    reshaped_image = image.reshape(-1, 3)
+
+    # Initialize centroids
+    centroids = init_centroids(k)
+
+    for _ in range(max_iterations):
+        # Assign pixels to the nearest centroid
+        labels = assign_to_centroids(reshaped_image, centroids)
+
+        # Update centroids based on the assigned pixels
+        new_centroids = update_centroids(reshaped_image, labels, k)
+
+        # Check for convergence (if centroids do not change)
+        if np.all(centroids == new_centroids):
+            break
+
+        centroids = new_centroids
+
+    # Create segmented image based on final labels
+    labels_2d = labels.reshape(image.shape[0], image.shape[1])
+    segmented_image = np.zeros_like(image)
+    for i in range(k):
+        segmented_image[labels_2d == i] = centroids[i]
+
+    return segmented_image
